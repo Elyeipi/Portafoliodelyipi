82d89d9ae9a67536290e9450fe5e8fc629fbd3aa
elyeipi <josepablomirandamunos@gmail.com>
2021-03-03 21:09:26 -0600

    Some js c:

---
+++ JavaScript/main.js
@@ -0,0 +1,14 @@
+let charging = document.querySelector(".chargingBar__porcentage");
+
+function LoadBar()
+{
+    let scrollTop = document.documentElement.scrollTop;
+    let distance = charging.offsetTop;
+    if(distance - 300 < scrollTop)
+    {
+        charging.classList.add("loading");
+    }
+}
+
+
+window.addEventListener('scroll', LoadBar)
