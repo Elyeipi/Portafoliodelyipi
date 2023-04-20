04902f799108aa1a3cf3f88324d032315dd9ca93
O Csm de JP <65045016+Elyeipi@users.noreply.github.com>
2023-04-20 13:53:06 -0600

    new changes idk if this shit's gonna work

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -29,6 +29,8 @@ const app = new PIXI.Application({ width: "1980", height: "1080" });
 
 // Containers
 const squareSprite = new PIXI.Graphics();
+const square1Sprite = new PIXI.Graphics();
+const square2Sprite = new PIXI.Graphics();
 
 
 /* Initializing */
@@ -44,9 +46,13 @@ squareSprite.beginFill(0xaa2233);
 squareSprite.drawRect(20, 20, 200, 200);
 squareSprite.endFill();
 
-squareSprite.filters = [new PIXI.filters.BlurFilter(50)];
+squareSprite.filters = [new PIXI.filters.BlurFilter(25)];
+square1Sprite.filters = [new PIXI.filters.BlurFilter(13)];
+square2Sprite.filters = [new PIXI.filters.BlurFilter(5)];
 
 app.stage.addChild(squareSprite);
+app.stage.addChild(square1Sprite);
+app.stage.addChild(square2Sprite);
 
 let elapsed = 0;
 
@@ -54,4 +60,6 @@ app.ticker.add(delta=>{
     elapsed += delta;
 
     squareSprite.x = (app.view.width / 2) + Math.tan(elapsed / 50) * 600;
+    square1Sprite.x = (app.view.width / 2) + Math.tan(elapsed / 50) * 600;
+    square2Sprite.x = (app.view.width / 2) + Math.tan(elapsed / 50) * 600;
 });
